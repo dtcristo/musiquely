d3d9567ce1b4e76909ef7887d93c9dac427caef2
David Cristofaro <git@dtcristo.com>
2015-04-15 21:01:44 +1000

    Set OmniAuth to use Rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+# Set OmniAuth to use Rails logger instead of STDOUT
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"], scope: 'playlist-read-private playlist-modify-public playlist-modify-private user-library-read user-library-modify user-read-private user-read-email'
 end
