ae815c49566a743f19f7b75f72c717cecf4a4bb3
David Cristofaro <git@dtcristo.com>
2015-04-16 22:52:18 +1000

    Add redirection to dashboard on session create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_auth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_url, notice: 'Signed in!'
+    redirect_to dashboard_path
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Signed out!'
+    redirect_to root_path, notice: 'Signed out!'
   end
 end
