91508bab4f348449b5cd906ca19eb5bed809fa6a
David Cristofaro <david@dtcristo.com>
2015-04-19 00:31:42 +1000

    Update SpotifyPlaylistsJob

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_auth(request.env['omniauth.auth'])
     # Asynchronously refresh the user's playlists
-    PlaylistsJob.perform_later(user)
+    SpotifyPlaylistsJob.perform_later(user)
     session[:user_id] = user.id
     redirect_to dashboard_path
   end
