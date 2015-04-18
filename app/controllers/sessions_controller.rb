f8aa5aa4d177abda1a17cf015ec646ab7489af2c
David Cristofaro <david@dtcristo.com>
2015-04-18 23:51:06 +1000

    Use ActiveJob to refresh user playlists in the background

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_auth(request.env['omniauth.auth'])
+    # Asynchronously refresh the user's playlists
+    PlaylistsJob.perform_later(user)
     session[:user_id] = user.id
     redirect_to dashboard_path
   end
