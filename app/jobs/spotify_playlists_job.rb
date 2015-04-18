c0469b52e3d4feefd27672546c7cde28ef801b5c
David Cristofaro <git@dtcristo.com>
2015-04-19 00:31:42 +1000

    Update SpotifyPlaylistsJob

--- app/jobs/spotify_playlists_job.rb
+++ app/jobs/spotify_playlists_job.rb
@@ -1,11 +1,18 @@
-class PlaylistsJob < ActiveJob::Base
+# Refresh the user's playlists from Spotify
+class SpotifyPlaylistsJob < ActiveJob::Base
   queue_as :default
 
   def perform(user)
-    spotify_user = user.spotify_user
+    # Build an array of Spotify playlists
+    spotify_playlists = get_playlists(user.spotify_user)
+    # Update or create the Playlist records
+    spotify_playlists.each do |spotify_playlist|
+      Playlist.update_or_create_from_spotify(spotify_playlist, user.id)
+    end
+  end
 
+  def get_playlists(spotify_user)
     spotify_playlists = []
-
     # Get all of the user's playlists, 50 at a time
     offset = 0
     loop do
@@ -17,10 +24,6 @@ class PlaylistsJob < ActiveJob::Base
       break if current_playlists.count < 50
       offset += 50
     end
-
-    # Update or create the playlist records
-    spotify_playlists.each do |spotify_playlist|
-      Playlist.update_or_create_from_spotify(spotify_playlist, user_id)
-    end
+    return spotify_playlists
   end
 end
