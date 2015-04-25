abbc1ab5e1581cecd3dbaa29023cedf0f1e960ae
David Cristofaro <git@dtcristo.com>
2015-04-25 23:44:45 +1000

    Add uniqueness constraints to spotify_id

--- db/migrate/20150204082702_create_tracks.rb
+++ db/migrate/20150204082702_create_tracks.rb
@@ -1,11 +1,13 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-      t.string :spotify_id, index: true
+      t.string :spotify_id
       t.string :name
       t.string :artist
 
       t.timestamps null: false
     end
+
+    add_index :tracks, :spotify_id, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20150419111329) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "playlists", ["spotify_id"], name: "index_playlists_on_spotify_id"
+  add_index "playlists", ["spotify_id"], name: "index_playlists_on_spotify_id", unique: true
 
   create_table "tracks", force: :cascade do |t|
     t.string   "spotify_id"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150419111329) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "tracks", ["spotify_id"], name: "index_tracks_on_spotify_id"
+  add_index "tracks", ["spotify_id"], name: "index_tracks_on_spotify_id", unique: true
 
   create_table "user_playlists", force: :cascade do |t|
     t.integer  "user_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150419111329) do
   end
 
   add_index "user_playlists", ["playlist_id"], name: "index_user_playlists_on_playlist_id"
+  add_index "user_playlists", ["user_id", "playlist_id"], name: "index_user_playlists_on_user_id_and_playlist_id", unique: true
   add_index "user_playlists", ["user_id"], name: "index_user_playlists_on_user_id"
 
   create_table "users", force: :cascade do |t|
@@ -61,6 +62,6 @@ ActiveRecord::Schema.define(version: 20150419111329) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "users", ["spotify_id"], name: "index_users_on_spotify_id"
+  add_index "users", ["spotify_id"], name: "index_users_on_spotify_id", unique: true
 
 end
