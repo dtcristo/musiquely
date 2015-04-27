596e06060ec968b7bf01616068ec9d1e87fca383
David Cristofaro <david@dtcristo.com>
2015-04-27 23:07:42 +1000

    Move Entry import logic into EntriesJob

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,21 +3,4 @@ class Entry < ActiveRecord::Base
   belongs_to :track
 
   validates :playlist_id, :track_id, presence: true
-
-  def self.import_from_spotify(spotify_tracks, playlist)
-    # Build values of each Entry
-    values = []
-    position = 0
-
-    spotify_tracks.each do |spotify_track|
-      track = Track.update_or_create_from_spotify(spotify_track)
-      values << [playlist.id, track.id, position += 1]
-    end
-
-    # Delete all Entries for this Playlist
-    Entry.where(playlist: playlist).delete_all
-
-    columns = [:playlist_id, :track_id, :position]
-    Entry.import(columns, values)
-  end
 end
