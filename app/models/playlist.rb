68070828fa60ced1df3c665d1f7f20369c526e1d
David Cristofaro <david@dtcristo.com>
2015-04-27 23:06:59 +1000

    Refactor UserPlaylistsJob to use Upsert

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -5,21 +5,4 @@ class Playlist < ActiveRecord::Base
   has_many :tracks, through: :entries
 
   validates :spotify_id, presence: true, uniqueness: true
-
-  def self.update_or_create_from_spotify(spotify_playlist)
-    playlist = find_by_spotify_id(spotify_playlist.id)
-    if (playlist)
-      playlist.update(name: spotify_playlist.name)
-    else
-      playlist = create_from_spotify(spotify_playlist)
-    end
-    return playlist
-  end
-
-  def self.create_from_spotify(spotify_playlist)
-    create! do |playlist|
-      playlist.spotify_id = spotify_playlist.id
-      playlist.name = spotify_playlist.name
-    end
-  end
 end
--- app/models/user_playlist.rb
+++ app/models/user_playlist.rb
@@ -1,5 +1,3 @@
-require 'upsert/active_record_upsert'
-
 class UserPlaylist < ActiveRecord::Base
   belongs_to :user
   belongs_to :playlist
@@ -8,30 +6,6 @@ class UserPlaylist < ActiveRecord::Base
   validates :user_id, :playlist_id, presence: true
   # The combination of [:playlist_id, :user_id] is unique
   validates :playlist_id, uniqueness: { scope: :user_id }
-
-  def self.upsert_from_spotify(spotify_playlists, user)
-    # Build values of each UserPlaylist
-    values = []
-
-    spotify_playlists.each do |spotify_playlist|
-      playlist = Playlist.update_or_create_from_spotify(spotify_playlist)
-      values << [user.id, playlist.id]
-    end
-
-    # Delete all UserPlaylists for this User
-    UserPlaylist.where(user: user).delete_all
-
-    columns = [:user_id, :playlist_id]
-    UserPlaylist.import(columns, values)
-
-    #Upsert.batch(Playlist.connection, :playlists) do |upsert|
-
-    #end
-
-    #spotify_playlist.each do |spotify_playlist|
-    #  Playlist.upsert({ spotify_id: spotify_playlist.id }, name: spotify_playlist.name)
-    #end
-  end
 
   def spotify_playlist
     RSpotify::Playlist.find(user.spotify_user.id, playlist.spotify_id)
