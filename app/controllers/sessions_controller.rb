ec06dd541b4bd2748d33606f55cc39dad757cece
David Cristofaro <david@dtcristo.com>
2015-05-06 22:47:09 +1000

    Use Upsert for User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_or_create_by_auth(request.env['omniauth.auth'])
+    user = User.upsert_with_auth(request.env['omniauth.auth'])
     # Asynchronously refresh the user's playlists
     UserPlaylistsJob.new.perform(user)
     session[:user_id] = user.id
